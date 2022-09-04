2d1dfe39d9b7e40f976477c59d2adf99faf8c937
Vincent Maggard <vincent@vincentmaggard.dev>
2022-09-04 02:07:35 -0400

    Added GPL header to file.

--- obplayer/streamer/metadata_updater.py
+++ obplayer/streamer/metadata_updater.py
@@ -7,6 +7,25 @@ import urllib.parse
 # not being able to handle a threat thats not running.
 import threading
 
+"""
+Copyright 2012-2015 OpenBroadcaster, Inc.
+
+This file is part of OpenBroadcaster Player.
+
+OpenBroadcaster Player is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenBroadcaster Player is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with OpenBroadcaster Player.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 class MetadataUpdater(threading.Thread):
     def __init__(self, protocol='http', host=None, port='8000', username='source', password='', mount=None):
         threading.Thread.__init__(self)
