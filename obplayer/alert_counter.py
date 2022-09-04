c53e37f657c1a86cae05541256c7a130d796e6ef
Vincent Maggard <vincent@vincentmaggard.dev>
2022-09-04 02:17:07 -0400

    Updated the copyright date range.

--- obplayer/alert_counter.py
+++ obplayer/alert_counter.py
@@ -2,6 +2,25 @@ import obplayer
 import pickle
 import os
 
+"""
+Copyright 2012-2022 OpenBroadcaster, Inc.
+
+This file is part of OpenBroadcaster Player.
+
+OpenBroadcaster Player is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenBroadcaster Player is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with OpenBroadcaster Player.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 class Alert_Counter(object):
     def __init__(self):
         self.datadir = obplayer.ObData.get_datadir()
