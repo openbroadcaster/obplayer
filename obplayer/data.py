ef5fffad84b2c7fe526bfbec90b5e7ade20b70c3
Vincent Maggard <vincent@vincentmaggard.dev>
2022-09-06 19:02:22 -0400

    Backported the station bug feature.

--- obplayer/data.py
+++ obplayer/data.py
@@ -341,6 +341,11 @@ class ObConfigData (ObData):
 
         self.add_setting('overlay_enable', '0', 'bool')
 
+        self.add_setting('bug_overlay_enable', '0', 'bool')
+        self.add_setting('bug_overlay_offset_x', '100', 'int')
+        self.add_setting('bug_overlay_offset_y', '100', 'int')
+        self.add_setting('bug_overlay_image', self.datadir + '/bug.png', 'text')
+
         self.add_setting('streamer_enable', '0', 'bool')
         self.add_setting('streamer_audio_in_mode', 'intersink', 'text')
         self.add_setting('streamer_audio_in_alsa_device', 'default', 'text')
