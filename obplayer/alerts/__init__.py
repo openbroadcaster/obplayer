3a380b957d114ea701ecf7f53b41ab71824fe562
Vincent Maggard <vincent@vincentmaggard.dev>
2023-03-22 19:51:21 -0400

    Added code to handle the attn tone volume.

--- obplayer/alerts/__init__.py
+++ obplayer/alerts/__init__.py
@@ -22,14 +22,23 @@ along with OpenBroadcaster Player.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import 
 
+import obplayer
 from obplayer.alerts.alert import ObAlert, parse_alert_file
 from obplayer.alerts.processor import ObAlertProcessor
+import os
 
 Processor = None
 
 def init():
     global Processor
     Processor = ObAlertProcessor()
+
+    vol = obplayer.Config.setting("alerts_attention_signal_volume", True)
+
+    if vol != None:
+        vol = round(1 / vol, 3)
+        print(vol)
+        os.system("ffmpeg -y -i obplayer/alerts/data/canadian-attention-signal.mp3 -filter:a \"volume={0}\" /tmp/attn.wav".format(str(vol)))
 
 def quit():
     pass
